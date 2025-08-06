a47ba9a7d7c3526c4000b1486e6244453913201b
vitor <viktorbruno2006@gmail.com>
2025-08-06 16:36:39 -0300

    Initial Commit

---
+++ Enquadra SwiftUI/Enquadra_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enquadra_SwiftUIApp.swift
+//  Enquadra SwiftUI
+//
+//  Created by Vítor Bruno on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Enquadra_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
